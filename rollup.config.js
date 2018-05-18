e759579f57c460de2462349cf1eb94d00bf10fd2
zeakd <artydeveloperduck@gmail.com>
2018-05-18 10:26:35 +0900

    set dependencies external

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import pkg from './package.json'
 export default [  
   {
     input: 'src/index.js',
+    external: [...Object.keys(pkg.dependencies)],
     output: [
       {
         file: pkg.main, 
